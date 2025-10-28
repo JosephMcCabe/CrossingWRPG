9a2419df4276bd71113f03a71d7302271b64cecd
Fatima Bazurto <bazurtofatima1@gmail.com>
2025-10-27 21:04:32 -0700

    Merge branch 'main' of https://github.com/JosephMcCabe/CrossingWRPG into Fatima2 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit. Request notification, activity, and location permissions at home screen. Implement timed notifications to map screen.

--- app/src/main/java/com/example/crossingwrpg/MainActivity.kt
+++ app/src/main/java/com/example/crossingwrpg/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.crossingwrpg
 
 import android.os.Bundle
-import android.Manifest
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
