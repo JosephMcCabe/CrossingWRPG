9dca5afed4b39941e1f055357d7acb59709e6cc9
Fatima Bazurto <bazurtofatima1@gmail.com>
2025-10-27 23:21:19 -0700

    Merge branch 'develop' of https://github.com/JosephMcCabe/CrossingWRPG into develop # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message abor # the commit Add notification class

---
+++ app/src/main/java/com/example/crossingwrpg/Notifications.kt
@@ -0,0 +1,56 @@
+package com.example.crossingwrpg
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.crossingwrpg.R.drawable.pixelpotion
+
+class Notifications(private val context: Context) {
+    private val levelUpChannelId = "level_up_channel"
+
+    fun initChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val levelUpChannel = NotificationChannel(
+                levelUpChannelId, "Level Ups", NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(levelUpChannel)
+        }
+    }
+
+    fun postLevelUp(messageText: String) {
+        val mainActivityIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val mainActivityPendingIntent = PendingIntent.getActivity(
+            context, 0, mainActivityIntent, PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val levelUpNotification = NotificationCompat.Builder(context, levelUpChannelId)
+            .setSmallIcon(pixelpotion)
+            .setContentTitle("You leveled up!")
+            .setContentText(messageText)
+            .setAutoCancel(true)
+            .setContentIntent(mainActivityPendingIntent)
+            .build()
+
+        val permissionNotGranted =
+            Build.VERSION.SDK_INT >= 33 &&
+                    ActivityCompat.checkSelfPermission(
+                        context, Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+
+        if (permissionNotGranted) return
+
+        NotificationManagerCompat.from(context).notify(1001, levelUpNotification)
+    }
+}
