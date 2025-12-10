367451f5a42f7e3460301a509147c568e8e02194
awesomepowner <132240422+awesomepowner@users.noreply.github.com>
2025-12-09 21:30:10 -0800

    Corrected music player on narrative screen and merged with latest develop

--- app/src/main/java/com/example/crossingwrpg/NarrativeScreen.kt
+++ app/src/main/java/com/example/crossingwrpg/NarrativeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,6 +24,9 @@ fun NarrativeScreen(
     navController: NavHostController
 ) {
 
+    val context = LocalContext.current
+
+    MusicPlayer.preparePlayer(context)
     MusicPlayer.changeSong("lostshrine")
     MusicPlayer.play()
 
