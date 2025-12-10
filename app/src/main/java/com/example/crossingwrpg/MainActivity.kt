4337f394a6a675989a20f3ee44a9b3eed7ac4c49
Joseph McCabe <Joseph.mccabe959@myci.csuci.edu>
2025-12-09 22:27:20 -0800

    make notificaiton bar transparent and change color of navigation bar

--- app/src/main/java/com/example/crossingwrpg/MainActivity.kt
+++ app/src/main/java/com/example/crossingwrpg/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
@@ -16,6 +17,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -60,6 +63,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         MusicPlayer.preparePlayer(applicationContext)
 
@@ -120,8 +125,10 @@ fun AppNavigation(
     // Scaffold provides overall screen structure
     Scaffold(
         modifier = modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets(0),
         bottomBar = {
-            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets,
+                containerColor = Color(0xFFD2B48C)) {
                 // Builds a navigation item for each defined destination
                 Destination.entries.forEach { destination ->
                     val isSelected = baseRoute == destination.route
